1586c82b2c8d4cc6404e6a1031935f066217d516
vachmin <pinkpinkhaseul@gmail.com>
2025-07-16 21:34:53 +0530

    Update core.py

--- pix_manip/core.py
+++ pix_manip/core.py
@@ -1,1 +1,27 @@
+from PIL import Image
+import random
+
+def load_image(path):
+    return Image.open(path)
+
+def scramble_pixels(img, key):
+    pixels = list(img.getdata())
+    random.Random(key).shuffle(pixels)
+    img.putdata(pixels)
+    return img
+
+def descramble_pixels(img, key):
+    width, height = img.size
+    pixels = list(img.getdata())
+    indexes = list(range(len(pixels)))
+    shuffled = indexes[:]
+    random.Random(key).shuffle(shuffled)
+
+    # Create reverse mapping
+    descrambled = [None] * len(pixels)
+    for original, shuffled_idx in enumerate(shuffled):
+        descrambled[shuffled_idx] = pixels[original]
+
+    img.putdata(descrambled)
+    return img
 
